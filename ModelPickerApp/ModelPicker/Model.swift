7ab88fef86b18b47a8f57549225653bcb8002c79
AIWASS23 <marcelodearaujo677@gmail.com>
2023-04-09 14:12:23 -0300

    RealityKit with SwiftUI

---
+++ ModelPickerApp/ModelPicker/Model.swift
@@ -0,0 +1,38 @@
+//
+//  Model.swift
+//  ModelPicker
+//
+//  Created by Marcelo de Araujo on 12/02/2023.
+//
+//
+
+import UIKit
+import RealityKit
+import Combine
+
+class Model {
+    var modelName: String
+    var image: UIImage
+    var modelEntity: ModelEntity?
+    
+    private var cancellable: AnyCancellable? = nil
+    
+    init(modelName: String) {
+        self.modelName = modelName
+        
+        // Thumbnail image is local so it will always be there
+        self.image = UIImage(named: modelName)!
+        
+        let filename = modelName + ".usdz"
+        self.cancellable = ModelEntity.loadModelAsync(named: filename)
+            .sink(receiveCompletion: { loadCompletion in
+                // Handle error
+                print("Debug: Unable to load modelEntity for modelName: \(self.modelName)")
+            }, receiveValue: { modelEntity in
+                // Get our modelEntity
+                self.modelEntity = modelEntity
+                print("Debug: Successfully loaded modelEntity for modelName: \(self.modelName)")
+            })
+            
+    }
+}
